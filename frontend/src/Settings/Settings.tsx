e108f2d02c0852782b2d79b9d6b0e0504995b570
nebolax <alexey.nebolsin.jr@gmail.com>
2023-01-27 22:20:59 +0200

    Design settings page

--- frontend/src/Settings/Settings.tsx
+++ frontend/src/Settings/Settings.tsx
@@ -1,5 +1,29 @@
+import { Box, Button, Grid, TextField } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import "./Settings.css";
+
 function Settings() {
-    return <h1>Settings component</h1>
+    const { t } = useTranslation('translation',  {keyPrefix: 'settings'});
+    return (
+        <Box display="flex" flexDirection="column" alignItems="center">
+            <h2>{t('title')}</h2>
+            <Box component="form">
+                <Box display="flex" flexDirection="row" justifyContent="space-between" className="form-label">
+                    <p className="label-wrapper">{t('leaders-per-page')}</p>
+                    <TextField type="number"/>
+                </Box>
+                <Box display="flex" flexDirection="row" justifyContent="space-between" className="form-label">
+                    <p className="label-wrapper">{t('projects-per-page')}</p>
+                    <TextField type="number"/>
+                </Box>
+                <Box display="flex" flexDirection="row" justifyContent="space-between" className="form-label">
+                    <p className="label-wrapper">{t('leader-comment-max-length')}</p>
+                    <TextField type="number"/>
+                </Box>
+                <Button variant="contained">{t('save')}</Button>
+            </Box>
+        </Box>  
+    );
 }
 
 export default Settings;
