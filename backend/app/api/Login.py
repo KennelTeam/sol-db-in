720f7f6e2123354af35fbfa7b13818b73ebba467
AndrusovN <andrusov.n@gmail.com>
2023-02-15 10:28:22 +0300

    documentation

--- backend/app/api/Login.py
+++ backend/app/api/Login.py
@@ -12,15 +12,21 @@ class Login(Resource):
         login = request.json.get('login')
         password = request.json.get('password')
         if not login or not password:
-            return jsonify(message='You should provide login and password'), 400
+            response = jsonify(message='You should provide login and password')
+            response.status_code = 400
+            return response
 
         user = User.get_by_login(login)
         if not user:
-            return jsonify(message='No such user'), 404
+            response = jsonify(message='No such user')
+            response.status_code = 404
+            return response
 
         user = user.auth(login, password)
         if not user:
-            return jsonify(message='Wrong password'), 401
+            response = jsonify(message='Wrong password')
+            response.status_code = 403
+            return response
 
         user.current_ip = request.remote_addr
         response = jsonify(role=user.role.name)
