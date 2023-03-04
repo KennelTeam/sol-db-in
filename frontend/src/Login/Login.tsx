4a7a75dc6baa1d9a9c31f4e76f7e10c13e2590f4
AndrusovN <andrusov.n@gmail.com>
2023-03-04 13:04:12 +0300

    axios example in Login.tsx

--- frontend/src/Login/Login.tsx
+++ frontend/src/Login/Login.tsx
@@ -1,22 +1,40 @@
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 import { Box, Button, IconButton, InputAdornment, TextField } from '@mui/material';
-import { useState } from 'react';
+import { SyntheticEvent, useState} from 'react';
 import { useTranslation } from 'react-i18next';
+import axios from "axios";
 
 
 function Login() {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [isError, setIsError] = useState(false);
   const {t} = useTranslation('translation', { keyPrefix: 'login' });
+
+  function on_submit(e: SyntheticEvent) {
+      setIsError(true);
+      e.preventDefault();
+      const target = e.target as typeof e.target & {
+          login: { value: string },
+          password: { value: string }
+      }
+      const request = {
+          login: target.login.value,
+          password: target.password.value
+      }
+      axios.post("http://127.0.0.1:5000/login", request)
+          .then((r) => {console.log(r)})
+  }
+
   return (
-      <Box display="flex" flexDirection="column" alignItems="center" onSubmit={(e) => {setIsError(true); e.preventDefault()}}>
+      <Box display="flex" flexDirection="column" alignItems="center" onSubmit={on_submit}>
         <h2>{t('title')}</h2>
         <Box component="form" display="flex" flexDirection="column" alignItems="center">
-          <TextField label={t('login')} sx={{width: "350px", marginBottom: "20px"}}/>
+          <TextField label={t('login')} name="login" sx={{width: "350px", marginBottom: "20px"}}/>
           <TextField
             label={t('password')}
             type={passwordVisible? "text": "password"}
             sx={{width: "350px", marginBottom: isError? "5px" : "20px"}}
+            name="password"
             InputProps={{
               endAdornment: (
                 <InputAdornment position="end">
