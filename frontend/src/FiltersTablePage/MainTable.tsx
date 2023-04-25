4ec5ae405a1264ae7f66f6fb4b673a83b0c9a73f
yveega <yveega@gmail.com>
2023-04-26 00:06:46 +0300

    Add sorting main table by numeric columns

--- frontend/src/FiltersTablePage/MainTable.tsx
+++ frontend/src/FiltersTablePage/MainTable.tsx
@@ -1,11 +1,11 @@
-import { TableCell, TableRow, TableHead, Table, List, ListItem, TableBody, Card, TableContainer, TablePagination } from '@mui/material'
+import { TableCell, TableRow, TableHead, Table, List, ListItem, TableBody, Card, TableContainer, TablePagination, TableSortLabel } from '@mui/material'
 import { Link } from 'react-router-dom'
 import CheckIcon from '@mui/icons-material/Check'
-import { Row, _getRows, _getColumns } from './_testFunctions'
+import { _getRows, _getColumns } from './_testFunctions'
 import { Box, styled } from '@mui/system'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { TableData } from './requests2API'
+import { visuallyHidden } from '@mui/utils'
 
 const StyledTableCell = styled(TableCell)({
     fontSize: '11px',
@@ -18,7 +18,85 @@ const StyledTableCell = styled(TableCell)({
     textAlign: "center"
 })
 
-function Head({ columnGroups }: { columnGroups: { name: string, columns: string[] }[] }) {
+function descendingComparator(a: Row, b: Row, orderBy: number) {
+    try {
+        const a_n = a.columns[orderBy].length > 0 ? Number(a.columns[orderBy][0].data) : 0,
+            b_n = b.columns[orderBy].length > 0 ? Number(b.columns[orderBy][0].data) : 0
+        if (b_n < a_n) {
+            return -1;
+        }
+        if (b_n > a_n) {
+            return 1;
+        }
+        return 0
+    } catch(e) {
+        console.log("Comparing exception:", e)
+        return 0
+    }
+}
+  
+type Order = 'asc' | 'desc';
+  
+function getComparator(
+        order: Order,
+        orderBy: number,
+    ): (
+        a: Row,
+        b: Row,
+    ) => number {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function stableSort(array: readonly Row[], comparator: (a: Row, b: Row) => number) {
+    const stabilizedThis = array.map((el, index) => [el, index] as [Row, number]);
+    console.log("stabilizedThis:", array, stabilizedThis)
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0])
+      if (order !== 0) {
+        return order;
+      }
+      return a[1] - b[1];
+    })
+    console.log("stabilizedThis sorted:", stabilizedThis)
+    return stabilizedThis.map((el) => el[0]);
+}
+
+export interface Row {
+    id: number,
+    columns: {
+        link?: string,
+        data: string
+    }[][]
+}
+
+export interface HeadColumn {
+    name: string,
+    numeric: boolean,
+    id: number
+}
+
+interface HeadProps {
+    columns: HeadColumn[],
+    order: Order,
+    orderBy: number
+    onRequestSort: (event: React.MouseEvent<unknown>, newOrderBy: number) => void
+}
+
+export interface TableData {
+    headColumns: HeadColumn[]
+    rows: Row[]
+}
+
+function Head(props: HeadProps) {
+
+    const { columns, order, orderBy, onRequestSort } = props
+
+    const createSortHandler =
+    (newOrderBy: number) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, newOrderBy);
+    }
 
     return (
         <TableHead>
@@ -26,12 +104,26 @@ function Head({ columnGroups }: { columnGroups: { name: string, columns: string[
                 <StyledTableCell style={{ fontWeight: "bold" }}>
                     id
                 </StyledTableCell>
-                {columnGroups.map((group) => (
-                    group.columns.map((columnName) => (
+                {columns.map(({ name, numeric, id }) => (
                     <StyledTableCell style={{ fontWeight: "bold" }}>
-                        {columnName}
+                        {numeric ? 
+                            (
+                                <TableSortLabel
+                                    active={orderBy === id}
+                                    direction={orderBy === id ? order : 'asc'}
+                                    onClick={createSortHandler(id)}
+                                >
+                                {name}
+                                {orderBy === id ? (
+                                    <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                    </Box>
+                                ) : null}
+                                </TableSortLabel>
+                            )
+                        : name}
                     </StyledTableCell>
-                ))))}
+                ))}
             </TableRow>
         </TableHead>
     )
@@ -80,6 +172,9 @@ export default function MainTable(props: TableData) {
 
     const [rowsPerPage, setRowsPerPage] = useState(10)
     const [page, setPage] = useState<number>(0)
+    const [order, setOrder] = useState<Order>('asc');
+    const [orderBy, setOrderBy] = useState<number>(0)
+    const [visibleRows, setVisibleRows] = useState<Row[]>([])
 
     const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
@@ -89,15 +184,52 @@ export default function MainTable(props: TableData) {
         setRowsPerPage(parseInt(event.target.value, 10));
         setPage(0);
     }
-    console.log(props.rows)
-    console.log(props.column_groups)
+    console.log(visibleRows)
+    console.log(props.headColumns)
+
+    const handleRequestSort = (event: React.MouseEvent<unknown>, newOrderBy: number) => {
+          const isAsc = orderBy === newOrderBy && order === 'asc';
+          const toggledOrder = isAsc ? 'desc' : 'asc';
+          setOrder(toggledOrder)
+          setOrderBy(newOrderBy)
+
+          console.log("handleRequestSort:", visibleRows)
+          const sortedRows = stableSort(visibleRows,
+            getComparator(toggledOrder, newOrderBy));
+          const updatedRows = sortedRows.slice(
+            page * rowsPerPage,
+            page * rowsPerPage + rowsPerPage,
+          );
+          console.log("Sorted Rows:", sortedRows)
+          setVisibleRows(updatedRows);
+        }
+
+    const head : HeadProps = {
+        columns: props.headColumns,
+        order: 'asc',
+        orderBy: 0,
+        onRequestSort: handleRequestSort
+    }
+
+    useEffect(() => {
+        console.log("useEffect:", props.rows)
+        const newRows : Row[] = props.rows.map(
+            (row) => (JSON.parse(JSON.stringify(row)))
+            )
+        if (props.rows.length > 0) {
+            console.log("New Rows:", JSON.parse(JSON.stringify(props.rows)))
+            setVisibleRows(JSON.parse(JSON.stringify(props.rows)))
+        }
+        console.log("visibleRows:", visibleRows)
+    }, [props.rows])
+
     return (
         <Box component={Card}>
             <TableContainer>
                 <Table size="small" stickyHeader>
-                    <Head columnGroups={props.column_groups}/>
+                    <Head {...head}/>
                     <TableBody>
-                        {props.rows
+                        {visibleRows
                             .slice(page * rowsPerPage, (page + 1) * rowsPerPage)
                             .map((row: Row) => (<RenderRow {...row}/>))}
                     </TableBody>
