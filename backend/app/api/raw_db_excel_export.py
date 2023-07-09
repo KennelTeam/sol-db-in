62922265bde3b91ec89375f744aca67dd35c8e80
nilovlev <nilovlev@yandex.ru>
2023-07-09 22:47:45 +0300

    fix linter errors

--- backend/app/api/raw_db_excel_export.py
+++ backend/app/api/raw_db_excel_export.py
@@ -6,7 +6,7 @@ from flask import Response, send_from_directory
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 
-from backend.app import FlaskApp
+from backend.app.flask_app import FlaskApp
 from backend.app.api.auxiliary import get_request
 from backend.app.database.user import Role
 from backend.constants import UPLOADS_DIRECTORY
@@ -21,7 +21,7 @@ class RawDbExcelExport(Resource):
     def get() -> Response:
         file_name = 'database.xlsx'
         connection = FlaskApp().db.session.connection()
-        with pd.ExcelWriter(os.path.join(UPLOADS_DIRECTORY, file_name), engine='xlsxwriter', engine_kwargs={'options': {'strings_to_urls': False}}) as writer:
+        with pd.ExcelWriter(os.path.join(UPLOADS_DIRECTORY, file_name), engine='xlsxwriter', engine_kwargs={'options': {'strings_to_urls': False}}) as writer:  # pylint: disable=abstract-class-instantiated
             for table_name in FlaskApp().db.metadata.tables.keys():
                 pd.read_sql_table(table_name, connection).to_excel(writer, sheet_name=table_name)
 
