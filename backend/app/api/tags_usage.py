7c70eac4fa994e8ddca5a4563886cf2f56ca83bf
AndrusovN <andrusov.n@gmail.com>
2023-09-03 12:34:37 +0300

    fix count

--- backend/app/api/tags_usage.py
+++ backend/app/api/tags_usage.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 
 from backend.app.api.auxiliary import get_request
-from backend.app.database import TagToAnswer, Form, Answer, Question
+from backend.app.database import TagToAnswer, Form, Answer, Question, FormattingSettings
 from backend.app.database.question_type import QuestionType
 from backend.app.database.user import Role
 from backend.app.flask_app import FlaskApp
@@ -20,10 +20,17 @@ class TagsUsage(Resource):
     @get_request(Role.ADMIN)
     def get() -> Response:
         tagged_answers = set(item.answer_id for item in TagToAnswer.query.all())
+        print(tagged_answers)
         tagging_questions = set(item.id for item in FlaskApp().request(Question).
                                 filter_by(_question_type=QuestionType.LONG_TEXT).all())
+        table_formattings = set(item.id for item in FlaskApp().request(FormattingSettings).filter(FormattingSettings._table_id != None).all())
+        table_questions = set(item.id for item in FlaskApp().request(Question).filter(Question._formatting_settings.in_(table_formattings)))
+        print(tagging_questions)
+        tagging_questions = tagging_questions - table_questions
         not_tagged_answers = FlaskApp().request(Answer).filter(Answer._question_id.in_(tagging_questions)).\
             filter(Answer.id.notin_(tagged_answers)).all()
+        for answer in not_tagged_answers:
+            print(json.dumps(answer.to_json()))
         forms = FlaskApp().request(Form).filter(Form.id.in_(set(item.form_id for item in not_tagged_answers))).all()
         links = [
             {
